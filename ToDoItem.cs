55bbd7a1d0fbbe8ae0fb3616d83ac746b3a9f2cf
Rubatronix94 <133864077+Rubatronix94@users.noreply.github.com>
2025-06-05 20:30:37 +0200

    Add files via upload

---
+++ ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool? IsCompleted { get; set; }
+    }
+}
